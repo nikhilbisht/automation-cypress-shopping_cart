b9cad043fed1479b5c0d111d1d7c5cbdc0b179d7
Nikhil Bisht <nikhilbisht96@gmail.com>
2022-01-01 14:22:26 +0530

    removed unused

--- cypress/integration/sortItem.spec.js
+++ cypress/integration/sortItem.spec.js
@@ -21,11 +21,7 @@ describe("Sort the items and Verify after sorting",()=>{
                 //verify the text Fress shipping
                 cy.get('div.shelf-stopper').should('have.text','Free shipping')
                 cy.get('img').should("be.exist")
-               
-                cy.get('div.shelf-item__price').within(()=>{
-                   
-                    cy.get('div.installment').contains('or 3 x$3.00')
-                })
+        
                 //Verify the text Add to cart
                 cy.get('div.shelf-item__buy-btn').should('have.text','Add to cart').click()
             })
